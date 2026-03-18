ea9de3fb5c7c5e08dfe3fbf68e553b09ca0b47e0
kidushaile <tup08857@temple.edu>
2026-03-18 15:34:24 -0400

    Added the second fragment and implemented the die roll process

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -19,10 +19,14 @@ class MainActivity : AppCompatActivity() {
 
 
 if (savedInstanceState == null) {
-    val fragment = DieFragment.newInstance(6)
+    val fragment1 = DieFragment.newInstance(10)
+    val fragment2 = DieFragment.newInstance(10)
     supportFragmentManager.beginTransaction()
-        .add(R.id.fragmentContainerView, fragment)
+        .add(R.id.fragmentContainerView, fragment1)
+        .add(R.id.fragmentContainerView2, fragment2)
         .commit()
+
+
 }
 
 
@@ -31,6 +35,9 @@ if (savedInstanceState == null) {
            findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
                supportFragmentManager.findFragmentById(R.id.fragmentContainerView)?.run {
                    (this as DieFragment).throwDie()
+                   supportFragmentManager.findFragmentById(R.id.fragmentContainerView2)?.run {
+                       (this as DieFragment).throwDie()
+                   }
                }
            }
 
